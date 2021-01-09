b13c7f81f42b30fc6b0eca4a3b1a14819940a4ae
snakehead007 <snakehead007@pm.me>
2021-01-09 14:49:59 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const pug = require("pug");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static(path.join(path.resolve(), "public")));
+app.engine("pug", pug.__express);
+app.set("views", path.join(path.resolve(), "views"));
+app.set("view engine", "pug");
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
